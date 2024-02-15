b3ad449e102f72a796509f8e16ec27a1bb48a0da
Serhiy Popovych <prossikmail@gmail.com>
2024-02-15 15:44:10 +0200

    added new checks

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/FlutterOverlayWindowPlugin.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/FlutterOverlayWindowPlugin.java
@@ -1,19 +1,18 @@
 package flutter.overlay.window.flutter_overlay_window;
-import java.io.StringWriter;
-import java.io.PrintWriter;
+
 import android.app.Activity;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
-import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationManagerCompat;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import io.flutter.FlutterInjector;
 import io.flutter.embedding.engine.FlutterEngine;
@@ -46,105 +45,122 @@ public class FlutterOverlayWindowPlugin implements
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
         try {
 
-        this.context = flutterPluginBinding.getApplicationContext();
-        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), OverlayConstants.CHANNEL_TAG);
-        channel.setMethodCallHandler(this);
+            this.context = flutterPluginBinding.getApplicationContext();
+            channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), OverlayConstants.CHANNEL_TAG);
+            channel.setMethodCallHandler(this);
 
-        messenger = new BasicMessageChannel(flutterPluginBinding.getBinaryMessenger(), OverlayConstants.MESSENGER_TAG,
-                JSONMessageCodec.INSTANCE);
-        messenger.setMessageHandler(this);
+            messenger = new BasicMessageChannel(flutterPluginBinding.getBinaryMessenger(), OverlayConstants.MESSENGER_TAG,
+                    JSONMessageCodec.INSTANCE);
+            messenger.setMessageHandler(this);
 
-        WindowSetup.messenger = messenger;
-        WindowSetup.messenger.setMessageHandler(this);
-    
-    } catch (Exception ex) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        ex.printStackTrace(pw);
-        String stackTraceString = sw.toString();
-        Log.d("OverLay", "onAttachedToEngine GetMessage:" + ex.getMessage());
-        Log.d("OverLay", "onAttachedToEngine stackTraceString:" + stackTraceString);
+            WindowSetup.messenger = messenger;
+            WindowSetup.messenger.setMessageHandler(this);
+
+        } catch (Exception ex) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            String stackTraceString = sw.toString();
+            Log.d("OverLay", "onAttachedToEngine GetMessage:" + ex.getMessage());
+            Log.d("OverLay", "onAttachedToEngine stackTraceString:" + stackTraceString);
 
 
-    }
+        }
     }
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-        pendingResult = result;
-        if (call.method.equals("checkPermission")) {
-            result.success(checkOverlayPermission());
-        } else if (call.method.equals("requestPermission")) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
-                intent.setData(Uri.parse("package:" + mActivity.getPackageName()));
-                mActivity.startActivityForResult(intent, REQUEST_CODE_FOR_OVERLAY_PERMISSION);
-            } else {
-                result.success(true);
-            }
-        } else if (call.method.equals("showOverlay")) {
-            try {
-            if (!checkOverlayPermission()) {
-                result.error("PERMISSION", "overlay permission is not enabled", null);
+        try {
+            pendingResult = result;
+            Log.d("OverLay", "onMethodCall, pendingResult:" + pendingResult);
+            Log.d("OverLay", "onMethodCall, call.method:" + call.method);
+
+            if (call.method.equals("checkPermission")) {
+                result.success(checkOverlayPermission());
+            } else if (call.method.equals("requestPermission")) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                    intent.setData(Uri.parse("package:" + mActivity.getPackageName()));
+                    mActivity.startActivityForResult(intent, REQUEST_CODE_FOR_OVERLAY_PERMISSION);
+                } else {
+                    result.success(true);
+                }
+            } else if (call.method.equals("showOverlay")) {
+
+                if (!checkOverlayPermission()) {
+                    result.error("PERMISSION", "overlay permission is not enabled", null);
+                    return;
+                }
+                Integer height = call.argument("height");
+                Integer width = call.argument("width");
+                String alignment = call.argument("alignment");
+                String flag = call.argument("flag");
+                String overlayTitle = call.argument("overlayTitle");
+                String overlayContent = call.argument("overlayContent");
+                String notificationVisibility = call.argument("notificationVisibility");
+                boolean enableDrag = call.argument("enableDrag");
+                String positionGravity = call.argument("positionGravity");
+
+                WindowSetup.width = width != null ? width : -1;
+                WindowSetup.height = height != null ? height : -1;
+                WindowSetup.enableDrag = enableDrag;
+                WindowSetup.setGravityFromAlignment(alignment != null ? alignment : "center");
+                WindowSetup.setFlag(flag != null ? flag : "flagNotFocusable");
+                WindowSetup.overlayTitle = overlayTitle;
+                WindowSetup.overlayContent = overlayContent == null ? "" : overlayContent;
+                WindowSetup.positionGravity = positionGravity;
+                WindowSetup.setNotificationVisibility(notificationVisibility);
+
+                final Intent intent = new Intent(context, OverlayService.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                context.startService(intent);
+                result.success(null);
+
+            } else if (call.method.equals("isOverlayActive")) {
+                result.success(OverlayService.isRunning);
                 return;
+            } else if (call.method.equals("closeOverlay")) {
+                if (OverlayService.isRunning) {
+                    final Intent i = new Intent(context, OverlayService.class);
+                    i.putExtra(OverlayService.INTENT_EXTRA_IS_CLOSE_WINDOW, true);
+                    context.startService(i);
+                    result.success(true);
+                }
+                return;
+            } else {
+                result.notImplemented();
             }
-            Integer height = call.argument("height");
-            Integer width = call.argument("width");
-            String alignment = call.argument("alignment");
-            String flag = call.argument("flag");
-            String overlayTitle = call.argument("overlayTitle");
-            String overlayContent = call.argument("overlayContent");
-            String notificationVisibility = call.argument("notificationVisibility");
-            boolean enableDrag = call.argument("enableDrag");
-            String positionGravity = call.argument("positionGravity");
+        } catch (Exception ex) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            String stackTraceString = sw.toString();
+            Log.d("OverLay", "onMethodCall GetMessage:" + ex.getMessage());
+            Log.d("OverLay", "onMethodCall stackTraceString:" + stackTraceString);
 
-            WindowSetup.width = width != null ? width : -1;
-            WindowSetup.height = height != null ? height : -1;
-            WindowSetup.enableDrag = enableDrag;
-            WindowSetup.setGravityFromAlignment(alignment != null ? alignment : "center");
-            WindowSetup.setFlag(flag != null ? flag : "flagNotFocusable");
-            WindowSetup.overlayTitle = overlayTitle;
-            WindowSetup.overlayContent = overlayContent == null ? "" : overlayContent;
-            WindowSetup.positionGravity = positionGravity;
-            WindowSetup.setNotificationVisibility(notificationVisibility);
 
-            final Intent intent = new Intent(context, OverlayService.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            context.startService(intent);
-            result.success(null);
-        }catch (Exception ex) {
-                StringWriter sw = new StringWriter();
-                PrintWriter pw = new PrintWriter(sw);
-                ex.printStackTrace(pw);
-                String stackTraceString = sw.toString();
-                Log.d("OverLay", "showOverlay GetMessage:" + ex.getMessage());
-                Log.d("OverLay", "showOverlay stackTraceString:" + stackTraceString);
-        
-        
-            }
-        } else if (call.method.equals("isOverlayActive")) {
-            result.success(OverlayService.isRunning);
-            return;
-        } else if (call.method.equals("closeOverlay")) {
-            if (OverlayService.isRunning) {
-                final Intent i = new Intent(context, OverlayService.class);
-                i.putExtra(OverlayService.INTENT_EXTRA_IS_CLOSE_WINDOW, true);
-                context.startService(i);
-                result.success(true);
-            }
-            return;
-        } else {
-            result.notImplemented();
         }
-
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-        channel.setMethodCallHandler(null);
-        WindowSetup.messenger.setMessageHandler(null);
-        Log.d("OverLay", "onDetachedFromEngine run()");
+        try {
+
+            channel.setMethodCallHandler(null);
+            WindowSetup.messenger.setMessageHandler(null);
+            Log.d("OverLay", "onDetachedFromEngine run()");
+        } catch (Exception ex) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            String stackTraceString = sw.toString();
+            Log.d("OverLay", "onDetachedFromEngine GetMessage:" + ex.getMessage());
+            Log.d("OverLay", "onDetachedFromEngine stackTraceString:" + stackTraceString);
+
+
+        }
+
 
     }
 
@@ -152,16 +168,16 @@ public class FlutterOverlayWindowPlugin implements
     public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         try {
             Log.d("OverLay", "onAttachedToActivity run:binding.getActivity()" + binding.getActivity());
-           
-        mActivity = binding.getActivity();
-        FlutterEngineGroup enn = new FlutterEngineGroup(context);
-        DartExecutor.DartEntrypoint dEntry = new DartExecutor.DartEntrypoint(
-                FlutterInjector.instance().flutterLoader().findAppBundlePath(),
-                "overlayMain");
-        FlutterEngine engine = enn.createAndRunEngine(context, dEntry);
-        FlutterEngineCache.getInstance().put(OverlayConstants.CACHED_TAG, engine);
-        binding.addActivityResultListener(this);
-            
+
+            mActivity = binding.getActivity();
+            FlutterEngineGroup enn = new FlutterEngineGroup(context);
+            DartExecutor.DartEntrypoint dEntry = new DartExecutor.DartEntrypoint(
+                    FlutterInjector.instance().flutterLoader().findAppBundlePath(),
+                    "overlayMain");
+            FlutterEngine engine = enn.createAndRunEngine(context, dEntry);
+            FlutterEngineCache.getInstance().put(OverlayConstants.CACHED_TAG, engine);
+            binding.addActivityResultListener(this);
+
         } catch (Exception ex) {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
@@ -179,8 +195,17 @@ public class FlutterOverlayWindowPlugin implements
 
     @Override
     public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-        Log.d("OverLay", "onReattachedToActivityForConfigChanges run:binding.getActivity()" + binding.getActivity());
-        this.mActivity = binding.getActivity();
+        try {
+            Log.d("OverLay", "onReattachedToActivityForConfigChanges run:binding.getActivity()" + binding.getActivity());
+            this.mActivity = binding.getActivity();
+        } catch (Exception ex) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            String stackTraceString = sw.toString();
+            Log.d("OverLay", "onReattachedToActivityForConfigChanges GetMessage:" + ex.getMessage());
+            Log.d("OverLay", "onReattachedToActivityForConfigChanges stackTraceString:" + stackTraceString);
+        }
     }
 
     @Override
@@ -191,19 +216,19 @@ public class FlutterOverlayWindowPlugin implements
     @Override
     public void onMessage(@Nullable Object message, @NonNull BasicMessageChannel.Reply reply) {
         try {
-        BasicMessageChannel overlayMessageChannel = new BasicMessageChannel(
-                FlutterEngineCache.getInstance().get(OverlayConstants.CACHED_TAG)
-                        .getDartExecutor(),
-                OverlayConstants.MESSENGER_TAG, JSONMessageCodec.INSTANCE);
-        overlayMessageChannel.send(message, reply);
-    } catch (Exception ex) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        ex.printStackTrace(pw);
-        String stackTraceString = sw.toString();
-        Log.d("OverLay", "onMessage GetMessage:" + ex.getMessage());
-        Log.d("OverLay", "onMessage stackTraceString:" + stackTraceString);
-    }
+            BasicMessageChannel overlayMessageChannel = new BasicMessageChannel(
+                    FlutterEngineCache.getInstance().get(OverlayConstants.CACHED_TAG)
+                            .getDartExecutor(),
+                    OverlayConstants.MESSENGER_TAG, JSONMessageCodec.INSTANCE);
+            overlayMessageChannel.send(message, reply);
+        } catch (Exception ex) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            String stackTraceString = sw.toString();
+            Log.d("OverLay", "onMessage GetMessage:" + ex.getMessage());
+            Log.d("OverLay", "onMessage stackTraceString:" + stackTraceString);
+        }
     }
 
     private boolean checkOverlayPermission() {
